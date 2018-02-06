6ccac3251bf88b1df0b53c95b5e2ab5e9a499f3a
khangit96 <khangit69@gmail.com>
2018-02-06 14:19:25 +0700

    dlsl

--- sound.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-import os
-
-# tts = gTTS(text='Tên gì hả?', lang='vi', slow=False)
-# tts.save("good.mp3")
-os.system("mpg321 nhacotrom.mp3")
-
--- testCrontab.py
+++
@@ -1,3 +0,0 @@
-f=open('/home/pi/Desktop/PythonOpenCV/log.txt','w')
-f.write('hi, iam startup')
-f.close()
